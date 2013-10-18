bc5a90576924598a9f682a34c3e731100fa77097
LML <ooaLML@googlemail.com>
2013-10-18 11:50:49 +0200

    testest

---
+++ GitHubTest/GitHubTest/src/core/Main.java
@@ -0,0 +1,23 @@
+package core;
+
+
+/**
+ * Test for GitHub.
+ *
+ * @author Marius Zächer
+ */
+public class Main
+{
+
+   /**
+    * Just a test.
+    *
+    * @param args
+    */
+   public static void main(String[] args)
+   {
+      System.out.println("Hello");
+      System.out.println("World!");
+   }
+
+}
